40c06e4e0ed0aad3cd350e7fc841b457c4370a2a
Mikulus6 <mikulus6@gmail.com>
2022-09-23 13:28:29 +0200

    Fixed setup.py script

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from os import walk
 from os.path import join
 from setuptools import find_packages,\
                        setup
@@ -11,10 +10,6 @@ version_file = join(directory, "version.txt")
 with open(version_file, 'r') as f:
     version = f.read()
 
-# include entire directory
-datafiles = [(d, [join(d, f) for f in files])
-             for d, folders, files in walk(directory)]
-
 setup(name="lacel",
       version=version,
       description="Łacel (Łoś assets conversions external library) is a library written in Python 3"
@@ -23,7 +18,8 @@ setup(name="lacel",
       author="Mikołaj Walc \"Mikulus\"",
       author_email="mikulus6@gmail.com",
       url="https://github.com/Mikulus6/lacel",
-      packages=find_packages(),
-      data_files=datafiles,
+      packages=find_packages(where=directory),
+      package_dir={"": directory},
+      include_package_data=True,
       install_requires=['Pillow>=9.2.0']
       )
